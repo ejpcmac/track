ac4d56679876ed2472fbd247c99d4049adfeb5af
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2020-08-02 08:07:43 +0500

    [bin] Move the tracking interface to `track info`

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,28 @@ use track::{Client, Config};
 /// A quick-and-dirty CLI tool for tracking parcels
 #[derive(Debug, StructOpt)]
 #[structopt(author = "Jean-Philippe Cugnet <jean-philippe@cugnet.eu>")]
-struct Opts {
+enum Command {
+    Info(Info),
+}
+
+/// Retrives and prints tracking info for a parcel
+#[derive(Debug, StructOpt)]
+struct Info {
     /// The tracking number
     #[structopt(name = "tracking_number")]
     tracking_number: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = Opts::from_args();
+    let command = Command::from_args();
 
-    let config = Config::load()?;
-    let client = Client::new(config);
-    client.track(&opts.tracking_number)?;
+    match command {
+        Command::Info(opts) => {
+            let config = Config::load()?;
+            let client = Client::new(config);
+            client.track(&opts.tracking_number)?;
+        }
+    }
 
     Ok(())
 }
