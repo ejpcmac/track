fa615c735a9728e294ececcbddef3f7ca82ad647
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-09-29 22:05:05 +0200

    build: generate proper version when installing via cargo install --git
    When installing via `cargo install --git`, only add the git revision for
    development versions.

--- build.rs
+++ build.rs
@@ -7,14 +7,19 @@ fn main() {
 /// Defines a variable containing the version with the Git revision.
 ///
 /// `VERSION_WITH_GIT` contains at least the cargo version, even when Git is not
-/// available. When Git is available, if the current Git tag and dirty state
-/// does not match *exactly* the cargo version prefixed by `v`, then the current
-/// Git revision and dirty state is added to the version as a tag.
+/// available. When Git is available, the current Git revision and dirty state
+/// is added to the version as a tag, except in the following cases:
+///
+/// * the build is done from a clean worktree checked out at a tag matching
+///   *exactly* the cargo version prefixed by `v`,
+/// * the build is done from a cargo checkout (via `cargo install --git`) and
+///   the version does not contain `-dev`.
 ///
 /// For instance:
 ///
 /// * Cargo version 1.0.0 on tag v1.0.0, clean state => `1.0.0`
-/// * Cargo version 1.0.0 on tag v1.0.0, dirty state => `1.0.0+abcd1234-modified`
+/// * Cargo version 1.0.0 on tag v1.0.0, dirty state =>
+///   `1.0.0+abcd1234-modified`
 /// * Cargo version 1.1.0-dev on any commit, clean state => `1.1.0-dev+abcd1234`
 fn define_version_with_git() {
     let cargo_version = env!("CARGO_PKG_VERSION");
@@ -25,11 +30,13 @@ fn define_version_with_git() {
 }
 
 fn version_with_git(cargo_version: &str) -> io::Result<String> {
-    if git_describe()? != format!("v{cargo_version}") {
+    if git_describe()? == format!("v{cargo_version}")
+        || is_cargo_checkout()? && !is_dev_version(cargo_version)
+    {
+        Ok(String::from(cargo_version))
+    } else {
         let revision = git_revision_and_state()?;
         Ok(format!("{cargo_version}+{revision}"))
-    } else {
-        Ok(String::from(cargo_version))
     }
 }
 
@@ -42,7 +49,7 @@ fn git_describe() -> io::Result<String> {
 
 fn git_revision_and_state() -> io::Result<String> {
     let revision = git_revision()?;
-    if git_is_dirty()? {
+    if git_is_dirty()? && !is_cargo_checkout()? {
         Ok(format!("{revision}-modified"))
     } else {
         Ok(revision)
@@ -61,4 +68,15 @@ fn git_is_dirty() -> io::Result<bool> {
         .args(&["status", "--porcelain"])
         .output()?;
     Ok(!output.stdout.is_empty())
+}
+
+fn is_cargo_checkout() -> io::Result<bool> {
+    let output = Command::new("git")
+        .args(&["status", "--porcelain"])
+        .output()?;
+    Ok(output.stdout == b"?? .cargo-ok\n")
+}
+
+fn is_dev_version(cargo_version: &str) -> bool {
+    cargo_version.contains("-dev")
 }
